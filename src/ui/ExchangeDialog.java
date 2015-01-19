f243553f76b87ba88f7bf93a4128682b1abc5093
Éric <ric@192.168.1.34>
2015-01-19 14:41:40 +0000

    Final moneyCalculator with URL exchangeRate

--- src/ui/ExchangeDialog.java
+++ src/ui/ExchangeDialog.java
@@ -3,8 +3,6 @@ package ui;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.Currency;
 import model.CurrencyMap;
 import model.Exchange;
